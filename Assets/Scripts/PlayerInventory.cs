4477ece3d733a37cf3ab0cec8f6df17a9e23cab9
mangang2 <qkqxhddl5206@gmail.com>
2024-11-06 14:35:43 +0900

    [Feat] 간단한 인벤토리와 수집 제작

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    //각각 아이템 개수를 저장하는 변수
+    public int crystalCount = 0;            //크리스탈 개수
+    public int plantCount = 0;              //식물 개수
+    public int bushCount = 0;               //수풀 개수
+    public int treeCount = 0;               //나무 개수
+
+    //아이템을 추가하는 함수, 아이템 종류에 따라서 해당 아이템의 개수를 증가 시킴
+    public void AddItem(ItemType itemType)
+    {
+        switch (itemType)
+        {
+            case ItemType.Crystal:
+                crystalCount++;    //크리스탈 개수 증가
+                Debug.Log($"크리스탈 획득 ! 현재 개수 : {crystalCount}");                //현재 크리스탈 개수 출력
+                break; 
+            case ItemType.Plant:
+                crystalCount++;    //식물 개수 증가
+                Debug.Log($"식물 획득 ! 현재 개수 : {plantCount}");                      //현재 식물 개수 출력
+                break;
+            case ItemType.Bush:
+                crystalCount++;    //수풀 개수 증가
+                Debug.Log($"수풀 획득 ! 현재 개수 : {bushCount}");                       //현재 수풀 개수 출력
+                break;
+            case ItemType.Tree:
+                crystalCount++;    //나무 개수 증가
+                Debug.Log($"나무 획득 ! 현재 개수 : {treeCount}");                       //현재 나무 개수 출력
+                break;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ShowInventory();
+        }
+    }
+
+    private void ShowInventory()
+    {
+        Debug.Log("=======인벤토리=======");
+        Debug.Log($"크리스탈:{crystalCount}개");
+        Debug.Log($"식물:{plantCount}개");
+        Debug.Log($"수풀:{bushCount}개");
+        Debug.Log($"나무:{treeCount}개");
+        Debug.Log("======================");
+    }
+}
